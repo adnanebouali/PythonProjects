3240602169d0f0d27523ca265fe3f77347b20eb3
adnanebouali <adnanbouali1@gmail.com>
2023-04-03 21:28:42 +0100

    update

---
+++ DailyPush/dailypush.py
@@ -0,0 +1,31 @@
+import datetime
+import subprocess
+from git import Repo
+
+# Step 1: Get the current date
+now = datetime.datetime.now()
+
+# Step 2: Write the date to the text file
+with open('D:\myfiles\AutoPush\Thefile.txt' , 'a') as file:
+    file.write(f"{now.strftime('%A')} // {now.strftime('%Y-%m-%d')} // {now.strftime('%H:%M:%S')} // {now.strftime('%j')} day of the year.\n")
+
+
+# Step 3: Use Git to commit and push the changes
+
+PATH_OF_GIT_REPO = r'D:\myfiles\AutoPush\.git' 
+COMMIT_MESSAGE = f"{now.strftime('%A')} // {now.strftime('%Y-%m-%d')}"
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+git_push()
+
+
+
