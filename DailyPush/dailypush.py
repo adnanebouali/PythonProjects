f5736ca1c908d6d11d528aa39e7eada61af892b6
adnanebouali <adnanbouali1@gmail.com>
2023-05-25 20:58:30 +0100

    update

--- DailyPush/dailypush.py
+++ DailyPush/dailypush.py
@@ -6,13 +6,13 @@ from git import Repo
 now = datetime.datetime.now()
 
 # Step 2: Write the date to the text file
-with open('D:\myfiles\AutoPush\Thefile.txt' , 'a') as file:
+with open('D:\myfiles\DailyPush\Thefile.txt' , 'a') as file:
     file.write(f"{now.strftime('%A')} // {now.strftime('%Y-%m-%d')} // {now.strftime('%H:%M:%S')} // {now.strftime('%j')} day of the year.\n")
 
 
 # Step 3: Use Git to commit and push the changes
 
-PATH_OF_GIT_REPO = r'D:\myfiles\AutoPush\.git' 
+PATH_OF_GIT_REPO = r'D:\myfiles\DailyPush\.git' 
 COMMIT_MESSAGE = f"{now.strftime('%A')} // {now.strftime('%Y-%m-%d')}"
 
 def git_push():
